2a7766c41487e487f1a9138a841cd7bda2c6f485
Maxamed Cigaal <lovemoha47@gmail.com>
2025-06-07 15:00:43 +0300

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useState } from 'react'
+
 import Layout from './pages/Layout'
 import Home from './pages/Home'
 import About from './pages/About';
